30a815409bf531d5aa4f1b53f3ac71087166aeb1
Brunodavies3 <brunodavies321@gmail.com>
2025-02-13 18:52:49 -0300

    Variaveis e funcoes no JS

---
+++ Aula0.1-variaveis-e-funcoes/JSAula01.js
@@ -0,0 +1,17 @@
+// leo e viado
+/*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa */
+
+const nome="Bruno";//precisa ter valor
+
+let Nome=""; //permite iniciar sem valor/vaziou
+
+var cor="azul";
+var cor="amarelo";
+
+let cidade="Registro";
+
+cidade = "Jacupiranga";
+
+const message="Hello world, Iniciando Java script";
+
+console.log(message);
